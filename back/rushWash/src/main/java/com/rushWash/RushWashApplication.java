a7793c6d72ebb41686e85a87cbcceb8a55935abb
soo0711 <k300hys@naver.com>
2025-04-25 15:08:49 +0900

    chore : 프로젝트 빌드

---
+++ back/rushWash/src/main/java/com/rushWash/RushWashApplication.java
@@ -0,0 +1,13 @@
+package com.rushWash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RushWashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RushWashApplication.class, args);
+	}
+
+}
---
+++ back/rushWash/src/test/java/com/rushWash/RushWashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rushWash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RushWashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
