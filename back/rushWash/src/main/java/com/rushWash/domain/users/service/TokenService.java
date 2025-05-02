9cea55e4aad116fbd893f347d717c6e22c87ee32
soo0711 <k300hys@naver.com>
2025-05-03 01:03:30 +0900

    refactor : Tokenservice로 userId 가져오게 수정 및 파일개행과 클래스명 수정

---
+++ back/rushWash/src/main/java/com/rushWash/domain/users/service/TokenService.java
@@ -0,0 +1,33 @@
+package com.rushWash.domain.users.service;
+
+import com.rushWash.common.response.CustomException;
+import com.rushWash.common.response.ErrorCode;
+import com.rushWash.global.security.jwt.JwtUtil;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class TokenService {
+    private final JwtUtil jwtUtil;
+
+    public int extractUserIdFromHeader(String authHeader) {
+        String token = parseToken(authHeader);
+
+        if (!jwtUtil.validateToken(token)) {
+
+            throw new CustomException(ErrorCode.INVALID_TOKEN);
+        }
+
+        return jwtUtil.getUserIdFromToken(token);
+    }
+
+    private String parseToken(String authHeader) {
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+
+            throw new CustomException(ErrorCode.INVALID_TOKEN);
+        }
+        return authHeader.replace("Bearer ", "");
+    }
+
+}
