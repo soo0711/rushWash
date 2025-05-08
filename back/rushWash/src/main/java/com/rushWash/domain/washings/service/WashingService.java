8e7fddd46d64e4c3da897b79f05abca0c90bb969
kimchaeyeon <kelly021128@naver.com>
2025-05-09 01:27:39 +0900

    김채연/[refactor] 사용자 분석내역 조회 response 수정

--- back/rushWash/src/main/java/com/rushWash/domain/washings/service/WashingService.java
+++ back/rushWash/src/main/java/com/rushWash/domain/washings/service/WashingService.java
@@ -2,10 +2,10 @@ package com.rushWash.domain.washings.service;
 
 import com.rushWash.common.response.CustomException;
 import com.rushWash.common.response.ErrorCode;
+import com.rushWash.domain.admin.washings.api.dto.response.WashingListResponse;
 import com.rushWash.domain.washings.api.dto.request.WashingEstimationRequest;
 import com.rushWash.domain.washings.api.dto.response.WashingDetailResponse;
 import com.rushWash.domain.washings.api.dto.response.WashingList;
-import com.rushWash.domain.washings.api.dto.response.WashingListResponse;
 import com.rushWash.domain.washings.domain.WashingHistory;
 import com.rushWash.domain.washings.domain.repository.WashingRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,9 +19,9 @@ public class WashingService {
 
     private final WashingRepository washingRepository;
 
-    public WashingListResponse getWashingListByUserId(int userId) {
-        List<WashingList> list = washingRepository.findItemsByUserId(userId);
-        return new WashingListResponse(list);
+    public List<WashingList> getWashingListByUserId(int userId) {
+
+        return  washingRepository.findItemsByUserId(userId);
 
     }
 
@@ -45,11 +45,11 @@ public class WashingService {
 
     }
 
-    public List<com.rushWash.domain.admin.washings.api.dto.response.WashingListResponse> getAdminWashingList () {
+    public List<WashingListResponse> getAdminWashingList () {
         return washingRepository.getWashingList();
     }
 
-    public List<com.rushWash.domain.admin.washings.api.dto.response.WashingListResponse> getAdminWashingGoodList () {
+    public List<WashingListResponse> getAdminWashingGoodList () {
         return washingRepository.getWashingGoodList();
     }
 
