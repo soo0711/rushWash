4f9df20711881e638fa5e42a73217c40517589c1
soo0711 <k300hys@naver.com>
2025-05-16 00:42:24 +0900

    feat : stain, label, stain-label 결과 분석 저장하는 controller와 service 구현

---
+++ back/rushWash/src/main/java/com/rushWash/domain/analysis/api/AnalysisRestController.java
@@ -0,0 +1,66 @@
+package com.rushWash.domain.analysis.api;
+
+import com.rushWash.common.response.ApiResponse;
+import com.rushWash.common.response.CustomException;
+import com.rushWash.common.response.ErrorCode;
+import com.rushWash.domain.analysis.service.AnalysisService;
+import com.rushWash.domain.users.service.TokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/analysis")
+public class AnalysisRestController {
+
+    private final AnalysisService analysisService;
+    private final TokenService tokenService;
+
+    // fastAPI와 연결해야함
+    @PostMapping("/stain")
+    public ApiResponse<String> getStainAnalysis(
+            @RequestHeader(name = "Authorization", required = false) String authHeader,
+            @RequestPart("file") MultipartFile file){
+
+        if (authHeader == null || authHeader.isEmpty()){
+            throw new CustomException(ErrorCode.INVALID_TOKEN);
+        }
+
+        int userId = tokenService.extractUserIdFromHeader(authHeader);
+        analysisService.getStainAnalysis(userId, file);
+
+        return ApiResponse.ok("임시 데이터 응답");
+    }
+
+    @PostMapping("/label")
+    public ApiResponse<String> getLabelAnalysis(
+            @RequestHeader(name = "Authorization", required = false) String authHeader,
+            @RequestPart("file") MultipartFile file){
+
+        if (authHeader == null || authHeader.isEmpty()){
+            throw new CustomException(ErrorCode.INVALID_TOKEN);
+        }
+
+        int userId = tokenService.extractUserIdFromHeader(authHeader);
+        analysisService.getLabelAnalysis(userId, file);
+
+        return ApiResponse.ok("임시 데이터 응답");
+    }
+
+    @PostMapping("/stain-label")
+    public ApiResponse<String> getStainAndLabelAnalysis(
+            @RequestHeader(name = "Authorization", required = false) String authHeader,
+            @RequestPart("file") MultipartFile file){
+
+        if (authHeader == null || authHeader.isEmpty()){
+            throw new CustomException(ErrorCode.INVALID_TOKEN);
+        }
+
+        int userId = tokenService.extractUserIdFromHeader(authHeader);
+        analysisService.getStainAndLabelAnalysis(userId, file);
+
+        return ApiResponse.ok("임시 데이터 응답");
+    }
+
+}
--- back/rushWash/src/main/java/com/rushWash/global/config/WebMvcConfig.java
+++ back/rushWash/src/main/java/com/rushWash/global/config/WebMvcConfig.java
@@ -1,2 +1,26 @@
-package com.rushWash.global.config;public class WebMvcConfig {
+package com.rushWash.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final String uploadPath;
+
+    public WebMvcConfig(@Value("${file.upload-path}") String uploadPath) {
+        this.uploadPath = uploadPath;
+    }
+
+    // 웹 이미지 path와 서버에 업로드 된 실제 이미지와 매핑 설정
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry
+                .addResourceHandler("/images/**") // web path: http://localhost/images/aaaa_1234354/gg.png
+                .addResourceLocations("file:///" + uploadPath + "/images/"); // 실제 이미지 파일 위치
+
+    }
 }
