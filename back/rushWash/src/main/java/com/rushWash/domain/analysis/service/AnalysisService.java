430358335ea4d909bd4062fbca67e0082ec5ba5a
soo0711 <k300hys@naver.com>
2025-05-21 01:43:45 +0900

    feat: label 분석 파이썬 연결

--- back/rushWash/src/main/java/com/rushWash/domain/analysis/service/AnalysisService.java
+++ back/rushWash/src/main/java/com/rushWash/domain/analysis/service/AnalysisService.java
@@ -2,8 +2,10 @@ package com.rushWash.domain.analysis.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rushWash.common.file.FileManagerService;
+import com.rushWash.common.file.dto.response.SaveFileResult;
 import com.rushWash.common.response.CustomException;
 import com.rushWash.common.response.ErrorCode;
+import com.rushWash.domain.analysis.api.dto.response.AnalysisOnlyLabelResponse;
 import com.rushWash.domain.analysis.api.dto.response.AnalysisOnlyStainResponse;
 import com.rushWash.domain.washings.domain.AnalysisType;
 import com.rushWash.domain.washings.domain.WashingHistory;
@@ -34,8 +36,10 @@ public class AnalysisService {
 
     public AnalysisOnlyStainResponse getStainAnalysis(int userId, MultipartFile file){
 
-        // 1. 실제 저장된 파일의 절대 경로 저장 후 불러오기
-        String absoluteImagePath = saveFileAndGetAbsolutePath(userId, file);
+        // 1. 파일 저장
+        SaveFileResult result = saveFileAndGetPaths(userId, file);
+        String savedFilePath = result.savedFilePath();
+        String absoluteImagePath = result.absoluteFilePath();
 
         // 2. 파이썬 불러오기
         String pythonOutput = executePythonScript("stain_only", absoluteImagePath);
@@ -74,8 +78,42 @@ public class AnalysisService {
         }
     }
 
-    public void getLabelAnalysis(int userId, MultipartFile file){
+    public AnalysisOnlyLabelResponse getLabelAnalysis(int userId, MultipartFile file){
+        // 1. 파일 저장
+        SaveFileResult result = saveFileAndGetPaths(userId, file);
+        String savedFilePath = result.savedFilePath();
+        String absoluteImagePath = result.absoluteFilePath();
 
+        // 2. 파이썬 불러오기
+        String pythonOutput = executePythonScript("label_only", absoluteImagePath);
+
+        // 3. JSON 응답값
+        try {
+            int jsonStart = pythonOutput.indexOf('{');
+            if (jsonStart == -1) {
+                throw new CustomException(ErrorCode.PYTHON_SCRIPT_OUTPUT_INVALID);
+            }
+            String jsonOutput = pythonOutput.substring(jsonStart);
+
+            AnalysisOnlyLabelResponse response = objectMapper.readValue(jsonOutput, AnalysisOnlyLabelResponse.class);
+
+            // WashingHistory와 WashingResult 저장
+            WashingHistory washingHistory = washingService.addWashingHistory(userId, AnalysisType.LABEL, savedFilePath, false);
+
+            List<String> detectedLabels = response.detectedLabels();
+            List<String> labelExplanation = response.labelExplanation();
+
+            for (int i = 0; i < detectedLabels.size(); i++) {
+                String category = detectedLabels.get(i);
+                String explanation = labelExplanation.get(i);
+                washingService.addWashingResult(washingHistory, category, explanation);
+            }
+
+
+            return response;
+        } catch (IOException e) {
+            throw new CustomException(ErrorCode.JSON_PARSING_FAILED);
+        }
     }
 
     public void getStainAndLabelAnalysis(int userId, MultipartFile file){
@@ -120,13 +158,14 @@ public class AnalysisService {
         }
     }
 
-    private String saveFileAndGetAbsolutePath(int userId, MultipartFile file) {
+    private SaveFileResult saveFileAndGetPaths(int userId, MultipartFile file) {
         // 1. 로컬에 저장
         String savedFilePath = fileManagerService.saveFile(userId, file);
         if (savedFilePath == null) {
             throw new CustomException(ErrorCode.FILE_SAVE_FAILED);
         }
-        // 2. 절대경로 return
-        return uploadPath + savedFilePath;
+        // 2. 실제 저장된 파일의 절대 경로
+        String absoluteFilePath = uploadPath + savedFilePath;
+        return new SaveFileResult(savedFilePath, absoluteFilePath);
     }
 }
